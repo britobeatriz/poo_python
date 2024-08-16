e3d32e5e458f13dc6b54f410476dedeeb8419201
Beatriz Brito <beatriz.debritolima1998@gmail.com>
2024-08-15 23:53:40 -0300

    clases y objetos

---
+++ clases y objetos.py
@@ -0,0 +1,13 @@
+# ejemplo
+
+class Celular():
+    marca = "samsung"
+    modelo = "S23"
+    camara = "48MP"
+
+# el objeto es una instancia de una clase
+
+celular1 = Celular()
+print(celular1.marca)
+
+# celular1 es un objeto y Celular() es una clase
